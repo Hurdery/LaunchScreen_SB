53a4ad708224fd935b683f75601e2559ef40c785
admin <fancl@doogo.local>
2019-11-20 16:57:52 +0800

    LSB 适配

---
+++ LaunchScreen/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LaunchScreen
+//
+//  Created by admin on 2019/11/20.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LaunchScreen/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  LaunchScreen
+//
+//  Created by admin on 2019/11/20.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    sleep(30);
+    return YES;
+}
+
+
+@end
